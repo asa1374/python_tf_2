0c6c66c0f91c0b43a78700ec1c5c1534e4212faa
asa1374 <asa1374@gmail.com>
2019-04-12 16:02:25 +0900

    final

--- app.py
+++ app.py
@@ -8,6 +8,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/calc')
+def calc():
+    sick = request.args.get('sick','0')
+    if (sick == '0'):
+        print('넘어온 식이 없다.')
+    else :
+        print('넘어온 식 : {}'.format(sick))
+        patt = '[0-9]+'
+        op = re.sub(patt,'',sick)
+        print('넘어온 연산자 : {}'.format(op))
+        nums = sick.split(op)
+        result = 0
+        if op == '+':
+            result = int(nums[0]) + int(nums[1])
+
+    print(result)
+
+    return jsonify(result = result)
 
 if __name__ == '__main__':
     app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,87 @@
 $(document).ready(()=>{
 
-    $('div button').click(function(){
-        alert($(this).text())
-    })
+    let mainOutput = $('#output');
+    let subOutput = $('#output2');
+    let op = $('#operator');
+    let num1 = $('#num1');
+    let num2 = $('#num2');
+    let temp = $('#temp');
+    let clearData = () =>{
+        num1.val('');
+        op.val('');
+        num2.val('');
+        temp.val('');
+    };
+    let clearOutput = () =>{
+        mainOutput.html('');
+        subOutput.html('');
+    };
+    let digitError = ()=>{
+        mainOutput.html(0);
+        subOutput.html('Reach Digit Limit');
+        temp.val(0);
+    };
+    $('.nums').click(function(){
+        if (('+-*/'.indexOf(mainOutput.html()) != -1)){
+            mainOutput.html('')
+        }
+        if ($(this).val()=='.'
+            && (mainOutput.html()).indexOf('.') != -1){
+            return;
+        }
+        if (mainOutput.html()=='0'
+            || subOutput.html() == 'Reach Digit Limit'){
+            clearOutput();
+        }
+        if (temp.val() !== ''){
+            clearOutput();
+            clearData();
+        }
+        mainOutput.append($(this).val());
+        subOutput.append($(this).val());
+        if (mainOutput.html().length > 12) {
+            digitError();
+        }
+    });
+    $('#clearButton').click(()=>{
+        mainOutput.html('0');
+        subOutput.html('');
+        clearData();
+    });
+    $('.btn-operate').click(function(){
 
-})
+        subOutput.append($(this).val());
+
+        mainOutput.html('');
+
+        $('.btn-equal').click(function(){
+            alert(subOutput.text())
+            let x = {sick : subOutput.text()}
+
+            $.getJSON($SCRIPT_ROOT+'/calc',x,d=>{
+                if (d.result.toString().length>13){
+                    digitError();
+                } else{
+                    alert(d.result)
+                }
+            });
+        });
+
+        /*let newOperator = $(this).val();
+        if (num1.val() !== ''
+            && ('+-*!/').indexOf(num1.val()) == -1
+            && op.val() !== ''){
+            num2.val(mainOutput.html());
+            if (('+-*!/').indexOf(num2.val()) != -1) return;
+            let x = {};
+            $.getJSON($SCRIPT_ROOT+'/calc',x,d=>{
+                if (d.result.toString().length>13){
+                    digitError();
+                } else{
+                    alert(d.result)
+                }
+            });
+
+        }*/
+    })
+});
